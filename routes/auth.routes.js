1bdb9066e38e72368581f047ac74eba5acb317cf
Vasiliy Yagunov <vasiliy.yagunov@mail.ru>
2020-08-13 21:47:50 +0300

    registration is server

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,5 @@
 const {Router} = require('express')
+const bcrypt = require('bcryptjs')
 const User = require('../models/User')
 const router = Router()
 
@@ -8,8 +9,21 @@ router.post('/register', async (req, res) => {
 
         const { email, password } = req.body
 
+        const candidate = await User.findOne({ email: email})
+
+        if (candidate) {
+            return res.status(400).json({ message: 'user exist'})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const user = new User({ email, password: hashedPassword })
+
+        await user.save()
+
+        res.status(201).json({ message: 'user is created' })
+
     } catch (e) {
-        res.status(500).json({ message: "Server rip..." })
+        res.status(500).json({ message: 'Server rip...' })
     }
 })
 
