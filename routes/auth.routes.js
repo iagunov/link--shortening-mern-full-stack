98188f188c5e6fbdb2496025a98d96103ab4d604
Vasiliy Yagunov <vasiliy.yagunov@mail.ru>
2020-08-13 22:06:18 +0300

    validation registration

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,26 @@
 const {Router} = require('express')
 const bcrypt = require('bcryptjs')
+const {check, validationResult} = require('express-validator')
 const User = require('../models/User')
 const router = Router()
 
 // /api/auth/register
-router.post('/register', async (req, res) => {
+router.post(
+    '/register',
+    [
+        check('email', 'incorrect email').isEmail(),
+        check('password', 'minimum password length is 6 characters').isLength({ min: 6 })
+    ],
+    async (req, res) => {
     try {
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: 'invalid data during registration'
+            })
+        }
 
         const { email, password } = req.body
 
