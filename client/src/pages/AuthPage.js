a2426f11cc160ce7f14c7ccc946ff2b2719e46e8
Vasiliy Yagunov <vasiliy.yagunov@mail.ru>
2020-08-16 23:19:18 +0300

    auth page

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -2,8 +2,39 @@ import React from 'react'
 
 export const AuthPage = () => {
     return (
-        <div>
-            <h1>Auth Page</h1>
+        <div className="row">
+            <div className="col s6 offset-s3">
+                <h1 className="white-text center">link shortening</h1>
+                <div className="card blue-grey darken-1">
+                    <div className="card-content white-text">
+                    <span className="card-title">authorization</span>
+                    <div>
+                        <div className="input-field">
+                            <input
+                             placeholder="enter email"
+                             id="email"
+                             type="text"
+                             name="email"
+                            />
+                            <label htmlFor="email">email</label>
+                        </div>
+                        <div className="input-field">
+                            <input
+                             placeholder="enter password"
+                             id="password"
+                             type="password"
+                             name="password"
+                            />
+                            <label htmlFor="email">password</label>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="card-action">
+                        <button className="btn green " style={{marginRight: 10}} >entrance</button>
+                        <button className="btn grey lighten-1">registration</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
