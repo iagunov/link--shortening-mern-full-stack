e1501037f53e2443bbe105d0ae0d9e472818a88c
Vasiliy Yagunov <vasiliy.yagunov@mail.ru>
2020-08-18 22:06:39 +0300

    http hook ready

--- client/src/hooks/http.hook.js
+++ client/src/hooks/http.hook.js
@@ -2,15 +2,29 @@ import {useState, useCallback} from 'react'
 
 export const useHttp = () => {
     const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(false)
+    const [error, setError] = useState(null)
 
     const request = useCallback ( async (url, method = 'GET', body = null, headers = {}) => {
+        setLoading(true)
         try {
+            const response = await fetch(url, {method, body, headers})
+            const data = await response.json()
 
+            if (!response.ok) {
+                throw new Error(data.message || 'something is wrong')
+            }
+
+            setLoading(false)
+
+            return data
         } catch (e) {
-            
+            setLoading(false)
+            setError(e.message)
+            throw e
         }
-    })
+    }, [])
 
-    return { loading, request, error}
+    const clearError = () => setError(null)
+
+    return { loading, request, error, clearError}
 }
